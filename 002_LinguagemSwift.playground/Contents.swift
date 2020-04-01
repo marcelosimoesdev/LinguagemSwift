add85a7b2e3094a62707ec5d897d23e4b4cd1de7
Marcelo Simoes <marcelosimoesdev@gmail.com>
2020-04-01 18:49:18 -0300

    Adaptação para Swift 5.

--- 002_LinguagemSwift.playground/Contents.swift
+++ 002_LinguagemSwift.playground/Contents.swift
@@ -1,7 +1,5 @@
 // Conceitos:
-
 // Integers
-
 var anoNascimento: Int = 1950
 
 Int8.min
@@ -13,13 +11,11 @@ UInt8.max
 
 
 // Floating-Point Numbers
-
 let salario: Float = 1000.0
 
 
 
 // Type Safety and Type Inference
-
 var idade = 50
 
 var tipoExplicito: String = "Olá"
@@ -30,7 +26,6 @@ let doubleInferido = 30.56
 
 
 // Numeric Literals
-
 let decimalInt = 17
 let binarioInt = 0b10001
 let octalInt = 0o21
@@ -46,7 +41,6 @@ let variavelNumerica = 0005_837.123
 
 
 // Numeric Type Conversion
-
 let inteiroPequeno: Int8 = 5
 let inteiroMedio: Int16 = 1_000
 
@@ -55,4 +49,3 @@ let somaInteirosDiferentes = Int16(inteiroPequeno) + inteiroMedio
 
 let valorDecimal = 2
 let valorConvertidoParaInt = Double(valorDecimal)
-
