d928e832c631cb7424ae87383b358fdc70bcd27b
Marcelo Simoes <marcelosimoesdev@gmail.com>
2020-04-01 21:25:06 -0300

    Adaptação para Swift 5.

--- 008_LinguagemSwift.playground/Contents.swift
+++ 008_LinguagemSwift.playground/Contents.swift
@@ -1,11 +1,8 @@
 // Apple Swift Tutorial - Do básico ao avançado (Parte 08)
-
 // Collection Types - Arrays
 
 
-
 // Declaração
-
 let paises1: Array<String> = ["México", "Japão", "Holanda"]
 
 let paises2: [String] = ["Brasil", "Alemanha", "Espanha"]
@@ -14,7 +11,7 @@ let paises3 = ["França", "China", "Canadá"]
 
 let todosPaises = paises1 + paises2 + paises3
 
-let arrayComValoresRepetidos = [Int](count: 5, repeatedValue: 10)
+let arrayComValoresRepetidos = Array(repeating: 10, count: 5)
 
 var arrayVazio: [String] = []
 
@@ -23,9 +20,7 @@ var arrayVazio: [String] = []
 // Mutabilidade x Imutabilidade (var x let) // NSArray - NSMutableArray
 
 
-
 // Acessar valores, pesquisar
-
 let primeiroItem = todosPaises[0]
 let ultimoItem = todosPaises[todosPaises.count - 1]
 let itemEspecifico = todosPaises[1]
@@ -35,39 +30,37 @@ todosPaises.count
 todosPaises.isEmpty
 
 
-if let indice = find(todosPaises, "Holanda") {
-    println("\(todosPaises[indice]) encontrada no array")
+if let indice = todosPaises.firstIndex(of: "Holanda") {
+    print("\(todosPaises[indice]) encontrada no array")
 }
 
 
 
 // Iteração
-
 for pais in todosPaises {
-    println(pais)
+    print(pais)
 }
 
-println("\n")
+print("\n")
 
-for (indice, valor) in enumerate(todosPaises) {
-    println("\(indice): \(valor)")
+for (indice, valor) in todosPaises.enumerated() {
+    print("\(indice): \(valor)")
 }
 
 
 
 // Manipulação
-
 var cores = ["Laranja", "Amarelo"]
 
 cores.append("Azul")
 //cores += "Verde" (No beta 5 do Xcode, o operador += não adiciona item mais. Apenas outro array.)
 cores += ["Branco", "Preto"]
-cores.insert("Roxo", atIndex: 1)
+cores.insert("Roxo", at: 1)
 
 cores[1...3] = ["Vermelho"]
 
-cores.removeAtIndex(1)
+cores.remove(at: 1)
 cores.removeLast()
 
-cores.removeAll(keepCapacity: false)
+cores.removeAll(keepingCapacity: false)
 cores = []
