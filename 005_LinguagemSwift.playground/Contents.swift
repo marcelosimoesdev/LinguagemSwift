3c06d0a902c345e12aa32ab183d248ef6300fafb
Marcelo Simoes <marcelosimoesdev@gmail.com>
2020-04-01 21:04:27 -0300

    Adaptação para Swift 5.

--- 005_LinguagemSwift.playground/Contents.swift
+++ 005_LinguagemSwift.playground/Contents.swift
@@ -1,10 +1,7 @@
 // Apple Swift Tutorial - Do básico ao avançado (Parte 05)
-
 // Assertions
-
 var idade = 18
 //idade = -18
-
 assert(idade >= 0, "Idade não pode ser menor que zero")
 
-println("Continuação do código...")
+print("Continuação do código...")
