da6a66a76ff002fc38556de92ada9c58fc7edd4b
Marcelo Simoes <marcelosimoesdev@gmail.com>
2020-04-01 21:47:20 -0300

    Adaptação para Swift 5.

--- 011_LinguagemSwift.playground/Contents.swift
+++ 011_LinguagemSwift.playground/Contents.swift
@@ -1,46 +1,41 @@
 // Apple Swift Tutorial - Do básico ao avançado (Parte 11)
-
 // Control Flow - Estruturas condicionais (if / switch)
-
 import Foundation
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
 // if
-
 var peso = 40.0
 
 if peso < 50.0 {
-    println("Você não pode doar sangue")
+    print("Você não pode doar sangue")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
 // if - else
-
 peso = 60.0
 if peso < 50.0 {
-    println("Você não pode doar sangue")
+    print("Você não pode doar sangue")
 } else {
-    println("Você pode doar sangue")
+    print("Você pode doar sangue")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
 // if's aninhados
-
 var salario = 3_000.00
 var aliquotaImpostoRenda: Float
 
@@ -56,7 +51,7 @@ if salario <= 1_787.77 {
     aliquotaImpostoRenda = 27.5
 }
 
-println("Alíquota: \(aliquotaImpostoRenda)%")
+print("Alíquota: \(aliquotaImpostoRenda)%")
 
 
 
@@ -66,8 +61,7 @@ println("Alíquota: \(aliquotaImpostoRenda)%")
 // switch
 
 
-
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -75,22 +69,22 @@ println("\n___________________________________\n")
 let ordemVogal = 2
 switch ordemVogal {
 case 1:
-    println("Vogal correspondente: A")
+    print("Vogal correspondente: A")
 case 2:
-    println("Vogal correspondente: E")
+    print("Vogal correspondente: E")
 case 3:
-    println("Vogal correspondente: I")
+    print("Vogal correspondente: I")
 case 4:
-    println("Vogal correspondente: O")
+    print("Vogal correspondente: O")
 case 5:
-    println("Vogal correspondente: U")
+    print("Vogal correspondente: U")
 default:
-    println("Não existe vogal correspondente para este número")
+    print("Não existe vogal correspondente para este número")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -98,16 +92,16 @@ println("\n___________________________________\n")
 let quantidadeFilhos = 4
 switch quantidadeFilhos {
 case 0...3:
-    println("Um carro comum serve")
+    print("Um carro comum serve")
 case 4...8:
-    println("Você vai precisar de uma Kombi")
+    print("Você vai precisar de uma Kombi")
 default:
-    println("Compre um ônibus")
+    print("Compre um ônibus")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -115,36 +109,36 @@ println("\n___________________________________\n")
 let letra = "a"
 switch letra {
 case "a", "e", "i", "o", "u":
-    println("É uma vogal")
+    print("É uma vogal")
 case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
 "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
-    println("É uma consoante")
+    print("É uma consoante")
 default:
-    println("Não é vogal nem consoante")
+    print("Não é vogal nem consoante")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
 // Condições (where)
 let console: NSString = "Playstation 4"
 switch console {
-case let nome where nome.containsString("Xbox"):
-    println("\(nome) é um console da Microsoft")
-case let nome where nome.containsString("Playstation"):
-    println("\(nome) é um console da Sony")
-case let nome where nome.containsString("Wii"):
-    println("\(nome) é um console da Nintendo")
+case let nome where nome.contains("Xbox"):
+    print("\(nome) é um console da Microsoft")
+case let nome where nome.contains("Playstation"):
+    print("\(nome) é um console da Sony")
+case let nome where nome.contains("Wii"):
+    print("\(nome) é um console da Nintendo")
 default:
-    println("Console não identificado")
+    print("Console não identificado")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -152,18 +146,18 @@ println("\n___________________________________\n")
 var coordenadas = (x: 0, y: 0)
 switch coordenadas {
 case (0, 0):
-    println("X e Y estão no eixo zero")
+    print("X e Y estão no eixo zero")
 case (0, _):
-    println("X está no eixo zero")
+    print("X está no eixo zero")
 case (_, 0):
-    println("Y está no eixo zero")
+    print("Y está no eixo zero")
 default:
-    println("X e Y não estão no eixo zero")
+    print("X e Y não estão no eixo zero")
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -171,13 +165,13 @@ println("\n___________________________________\n")
 coordenadas = (3, 5)
 switch coordenadas {
 case (0, 0):
-    println("X e Y estão no eixo ZERO")
+    print("X e Y estão no eixo ZERO")
 case (0, let y):
-    println("X está no eixo ZERO e Y está no eixo \(y)")
+    print("X está no eixo ZERO e Y está no eixo \(y)")
 case (let x, 0):
-    println("X está no eixo \(x) e Y está no eixo ZERO")
+    print("X está no eixo \(x) e Y está no eixo ZERO")
 case (let x, let y): // Este "case" substitui o default
-    println("X está no eixo \(x) e Y está no eixo \(y)")
+    print("X está no eixo \(x) e Y está no eixo \(y)")
 }
 
 
@@ -188,8 +182,7 @@ case (let x, let y): // Este "case" substitui o default
 // Control Transfer Statements
 
 
-
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -204,28 +197,28 @@ for caracter in stringEntrada {
         stringSaida.append(caracter)
     }
 }
-println(stringSaida)
+print(stringSaida)
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 // break
 let letra1 = "a"
 switch letra1 {
 case "a", "e", "i", "o", "u":
-    println("É uma vogal")
+    print("É uma vogal")
 case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
 "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
-    println("É uma consoante")
+    print("É uma consoante")
 default:
     break
 }
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -239,11 +232,11 @@ case 2, 3, 5, 7, 11, 13, 17, 19:
 default:
     descricao += " um inteiro."
 }
-println(descricao)
+print(descricao)
 
 
 
-println("\n___________________________________\n")
+print("\n___________________________________\n")
 
 
 
@@ -260,4 +253,4 @@ loopTexto: for letra in textoEntrada {
         textoSaida.append(letra)
     }
 }
-println(textoSaida)
+print(textoSaida)
