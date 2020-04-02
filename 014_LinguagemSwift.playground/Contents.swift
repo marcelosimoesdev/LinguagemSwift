12ccfbf41747e42d5f80fef841236adcafa7f835
Marcelo Simoes <marcelosimoesdev@gmail.com>
2020-04-01 22:25:02 -0300

    Adaptação para Swift 5.

--- 014_LinguagemSwift.playground/Contents.swift
+++ 014_LinguagemSwift.playground/Contents.swift
@@ -1,7 +1,5 @@
 // Linguagem Swift do básico ao avançado (Parte 14)
-
 // Closures
-
 
 
 func decrescente(v1: Int, v2: Int) -> Bool {
@@ -11,64 +9,59 @@ func decrescente(v1: Int, v2: Int) -> Bool {
 
 let numeros = [4, 1, 3, 2, 5]
 
-let numerosDecrescentes = numeros.sorted(decrescente)
+let numerosDecrescentes = numeros.sorted(by: decrescente)
 
 
 
 // Sintaxe Closure
 /*
-
     { ("parametros") -> "tipo retorno" in
         "código"
     }
-
 */
 
 
 
 // Observações sobre Closures:
-
     // Não possuem um nome como as funções.
     // Não podem definir valor padrão para parâmetros.
 
 
-
 // Exemplo
-let numerosDecrescentes1 = numeros.sorted( { (v1: Int, v2: Int) -> Bool in return v1 > v2 } )
+let numerosDecrescentes1 = numeros.sorted(by: { (v1: Int, v2: Int) -> Bool in return v1 > v2 } )
 
 // Inferindo o tipo dos parâmetros e retorno
-let numerosDecrescentes2 = numeros.sorted( { v1, v2 in return v1 > v2 } )
+let numerosDecrescentes2 = numeros.sorted(by: { v1, v2 in return v1 > v2 } )
 
 // Removendo o "return"
-let numerosDecrescentes3 = numeros.sorted( { v1, v2 in v1 > v2 } )
+let numerosDecrescentes3 = numeros.sorted(by: { v1, v2 in v1 > v2 } )
 
 // Removendo parâmetros e "in"
-let numerosDecrescentes4 = numeros.sorted( { $0 > $1 } )
+let numerosDecrescentes4 = numeros.sorted(by: { $0 > $1 } )
 
 // Simplificando mais ainda
-let numerosDecrescentes5 = numeros.sorted(>)
+let numerosDecrescentes5 = numeros.sorted(by: >)
 
 
 
 // Trailing Closures
-
 import Foundation
 
-func executarClosureApos(#tempo: UInt32, closure: () -> ()) {
+func executarClosureApos(tempo: UInt32, closure: () -> ()) {
 
-    println("Aguardando \(tempo)s")
+    print("Aguardando \(tempo)s")
     sleep(tempo)
     closure()
 }
 
 // Passando a closure DENTRO dos parênteses
-executarClosureApos(tempo: 1, {
-    println("Dentro")
+executarClosureApos(tempo: 1, closure: {
+    print("Dentro")
 })
 
 // Passando a closure FORA dos parênteses
 executarClosureApos(tempo: 2) {
-    println("Fora")
+    print("Fora")
 }
 
 
@@ -79,14 +72,13 @@ func funcaoSoComUmParametro(closure: () -> ()) {
 }
 
 funcaoSoComUmParametro {
-    println("Qualquer coisa...")
+    print("Qualquer coisa...")
 }
 
 
 
 // Contexto
-
-func criarIncrementador(valorIncremento: Int) -> () -> Int {
+func criarIncrementador(_ valorIncremento: Int) -> () -> Int {
     
     var valorTotal = 0
     
@@ -104,27 +96,19 @@ let outroIncrementoDeTres = criarIncrementador(3)
 incrementoDeTres() // 3
 incrementoDeTres() // 6
 incrementoDeTres() // 9
-
 outroIncrementoDeTres() // 3
 outroIncrementoDeTres() // 6
 outroIncrementoDeTres() // 9
 
 
-
 // Closures são passados por referência
-
 let incrementoDeCinco = criarIncrementador(5)
 let mesmoIncrementoDeCinco = incrementoDeCinco
 
 incrementoDeCinco() // 5
 incrementoDeCinco() // 10
 incrementoDeCinco() // 15
-
 mesmoIncrementoDeCinco() // 20
 mesmoIncrementoDeCinco() // 25
 mesmoIncrementoDeCinco() // 30
-
 incrementoDeCinco() // 35
-
-
-
